ab11f744ba25a9400393ea0488aa844794944a8c
ehsc <esucasacac@unsa.edu.pe>
2023-01-27 11:42:41 -0500

    elimina codigo no necesario

--- app.js
+++ app.js
@@ -84,72 +84,9 @@ function desencriptar(){
         ocultar(textoInsertado);
     }    
 }
-///funcion compara minuscuas
-
-/*
-let palabra = "ayeyeye";
-
-if(palabra.match(/^[a-z]+$/)){
-    console.log("minusculas");
-}else{
-    console.log("No minusculas");
-}
-*/
 
 button.addEventListener("click",encriptar);
 button2.addEventListener("click",desencriptar);
-
-
-/*
-button.addEventListener("click", function(){
-    ocultar(muneco);
-    ocultar(p1);
-    ocultar(p2);
-    mostrar(textoInsertado);
-    mostrar(copiarBoton);
-    const textareaValue = document.getElementById("txt");
-    const rpta = textareaValue.value;
-    var rptatxt = rpta.replace(/a|e|i|o|u/g, function coincidencia(x){
-        if(x === "a"){
-            return "ai";
-        }else if(x === "e"){
-            return "enter";
-        }else if(x === "i"){
-            return "imes";
-        }else if(x === "o"){
-            return "ober";
-        }else if(x === "u"){
-            return "ufat";
-        }
-    });
-    txtCuadro.innerHTML = rptatxt;
-});
-
-button2.addEventListener("click", function(){
-    ocultar(muneco);
-    ocultar(p1);
-    ocultar(p2);
-    mostrar(textoInsertado);
-    mostrar(copiarBoton);
-    const textareaValue = document.getElementById("txt");
-    const rpta = textareaValue.value;
-    var rptatxt = rpta.replace(/ai|enter|imes|ober|ufat/g, function coincidencia(x){
-        if(x === "ai"){
-            return "a";
-        }else if(x === "enter"){
-            return "e";
-        }else if(x === "imes"){
-            return "i";
-        }else if(x === "ober"){
-            return "o";
-        }else if(x === "ufat"){
-            return "u";
-        }
-    });
-    txtCuadro.innerHTML = rptatxt;
-});
-
-*/
 
 button3.addEventListener("click",function(){
     const textareaValue = document.getElementById("textoInsertado");
